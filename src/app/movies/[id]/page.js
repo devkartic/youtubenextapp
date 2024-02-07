bccae2b72ec6eec39c91dae80cc3a7347e09c584
devkartic <kartic.gharami@gmail>
2024-02-08 00:19:43 +0600

    working with movies list and details

--- src/app/movies/[id]/page.js
+++ src/app/movies/[id]/page.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import styles from "@/app/styles/common.module.css";
+import Image from "next/image";
+import Link from "next/link";
+import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
 
-const Page = () => {
+const Page = async ({params}) => {
+
+    const id = params.id;
+
+    new Promise(resolve => setTimeout(resolve, 2000));
+
+    const env = process.env;
+    const url = env.RAPID_API_URL+`/title/details/?ids=${id}&lang=en`;
+    const key = env.RAPID_API_KEY;
+    const host = env.RAPID_API_HOST;
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': key,
+            'X-RapidAPI-Host': host
+        }
+    };
+
+    const response = await fetch(url, options);
+    const result = await response.json();
+    const movie = result[0].details;
+
     return (
         <>
-            <h2>Routing in next.js</h2>
+            <div className={styles.container}>
+                <h2 className={styles.movie_title}>Netflix \ <span>{movie.type}</span></h2>
+                <div className={styles.card}>
+                    <div className={styles.card_image}>
+                        <Image src={movie.backgroundImage.url} alt={movie.title} width={260} height={200}/>
+                    </div>
+                    <div className={styles.card_data}>
+                        <h2>{movie.title}</h2>
+                        <p>
+                            {movie.synopsis}
+                        </p>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
--- src/app/movies/page.js
+++ src/app/movies/page.js
@@ -1,13 +1,46 @@
 import React from 'react';
 import Link from "next/link";
+import process from "next/dist/build/webpack/loaders/resolve-url-loader/lib/postcss";
+import MovieCard from "@/app/components/MovieCard";
+import styles from "@/app/styles/common.module.css"
 
-const Page = () => {
+const Movies = async () => {
+
+    await new Promise(resolve => setTimeout(resolve, 2000));
+
+    const env = process.env;
+    const url = env.RAPID_API_URL+`/search/?query=stranger&offset=0&limit_titles=50&limit_suggestions=20&lang=en`;
+    const key = env.RAPID_API_KEY;
+    const host = env.RAPID_API_HOST;
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': key,
+            'X-RapidAPI-Host': host
+        }
+    };
+
+    const response = await fetch(url, options);
+    const result = await response.json();
+    const movies = result.titles;
+
     return (
         <>
-            <h1>Movies Page</h1>
-            <Link href={'/movies/2'}>Click here</Link>
+            <section className={styles.movieSection}>
+                <div className={styles.container}>
+                    <h1>Series & Movies</h1>
+                    <div className={styles.card_section}>
+                        {
+                            movies.map((currentElement) => {
+                                return <MovieCard key={currentElement.id} {...currentElement}/>
+                            })
+                        }
+                    </div>
+                </div>
+            </section>
         </>
     );
 };
 
-export default Page;
+export default Movies;
